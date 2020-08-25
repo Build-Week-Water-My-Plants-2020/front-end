7d42b5f2a2b42200c4de9e0a0ec3eb7e7df91fd0
Jonathan Gatz <jonathangatz@Jonathans-MBP.attlocal.net>
2020-08-25 00:02:09 -0400

    added img upload, eit profile, & profile info components

--- plant-build/src/components/Plants.js
+++ plant-build/src/components/Plants.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
-export default class SignUp extends Component {
+export default class Profile extends Component {
     render() {
         return (
             <form>
-                <h3>Sign Up</h3>
+                <h3>My Profile Info</h3>
 
                 <div className="form-group">
                     <label>Username</label>
@@ -16,15 +16,11 @@ export default class SignUp extends Component {
                     <input type="text" className="form-control" placeholder="Phone" />
                 </div>
 
+
                 <div className="form-group">
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
-                </p>
             </form>
         );
     }
