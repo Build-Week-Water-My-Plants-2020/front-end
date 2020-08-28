24746907943be064352546efc39391ccb0e45c0f
Chanis Torres <wonderland@Chaniss-MacBook-Pro.local>
2020-08-28 02:17:47 -0400

    update

--- plant-build/src/components/AddingPlant.js
+++ plant-build/src/components/AddingPlant.js
@@ -1,32 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import {PlantContext} from '../context/PlantContext';
 
 const AddPlant = props => {
+    const { frequency, setFrequency } = useContext(PlantContext);
     const [addPlant, setAddPlant] = useState({
         user_id: 1,
         nickname: '',
         species: '',
-        h2ofrequency: ''
+        h2ofrequency: 1
     });
+    const[notify, setNotify] = useState(false);
+
     const handleAddChanges = e => {
         setAddPlant({ ...addPlant, [e.target.name]: e.target.value});
     };
+
     const postPlant = (add) => {
         axiosWithAuth()
         .post(`/plants/${addPlant.user_id}`, add)
         .then(res => {
             console.log('postPlant *success*', res);
             props.getPlants();
+            setFrequency([...frequency, {frequency: addPlant.h2ofrequency, plant: addPlant}]);
         })
         .catch(err => 
-            console.log('postPlant *failure*', err))
-    }
+            console.log('postPlant *failure*', err)
+        )
+    };
+
     const postSubmitHandler = e => {
         e.preventDefault();
         postPlant(addPlant);
-    }
+        const time = addPlant.h2ofrequency * 2000;
+        setTimeout(() => {
+            setNotify(true);
+        }, time);
+    };
 
     return (
+        <div>
+            {notify ?
+            <div onClick={() => setNotify(false)}
+            className='alert'><span><i class="far fa-bell"></i></span>{addPlant.nickname} is thirsty!</div> : null}
+       
         <form onSubmit = {postSubmitHandler}>
             <h3>add a plant</h3>
             <div>
@@ -49,8 +66,8 @@ const AddPlant = props => {
             </div>
             <div>
             <div>
-                    <label>Watering frequency</label>
-                    </div>
+            <label>Watering frequency</label>
+            </div>
             <input
             name='h2ofrequency'
             type='number'
@@ -65,6 +82,7 @@ const AddPlant = props => {
             <button type="submit" className="btn btn-primary btn-block">Submit</button>
         
         </form>
+        </div>
     );
 };
 
